4b74a53caf2ec79acab18a881e9a4f8e437284c3
Dave Hrycyszyn <futurechimp@users.noreply.github.com>
2019-11-04 14:40:46 +0000

    First test!

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+use curve25519_dalek::scalar::Scalar;
+use rand_os;
+
 struct Address {}
 struct Delay {}
 pub struct Destination {}
@@ -14,21 +17,39 @@ struct Host {
 
 pub struct SenderSecret {}
 struct SphinxHeader {}
-pub struct SphinxPacket {}
+pub struct SphinxPacket {
+    header: SphinxHeader,
+    payload: Vec<u8>,
+}
 struct SharedKey {}
 
+// TODO: a utility function to turn this into properly concatenated bytes
 pub fn create_packet(message: Vec<u8>, route: Vec<Hop>) -> SphinxPacket {
     let (header, shared_keys) = create_header(route);
-    let payload = create_enc_payload(message, shared_keys);
-    // let packet: SphinxPacket = header.to_bytes || payload.to_bytes
-    // return packet;
-    SphinxPacket {}
+    let enc_payload = create_enc_payload(message, shared_keys);
+    let packet = SphinxPacket {
+        header,
+        payload: enc_payload,
+    };
+    packet
 }
 
-// needs a secret key somehow
+// needs the processor's secret key somehow, figure out where this will come from
 // the return value could also be a message, handle this
-fn unwrap_layer(packet: SphinxPacket) -> (SphinxPacket, Address, Delay) {
-    return (SphinxPacket {}, Address {}, Delay {});
+fn unwrap_layer(packet: SphinxPacket) -> (SphinxPacket, Hop) {
+    return (
+        SphinxPacket {
+            header: SphinxHeader {},
+            payload: vec![],
+        },
+        Hop {
+            host: Host {
+                address: Address {},
+                pub_key: String::from(""),
+            },
+            delay: Delay {},
+        },
+    );
 }
 
 // header: SphinxHeader, enc_payload: Vec<u8>
@@ -44,6 +65,11 @@ fn create_header(route: Vec<Hop>) -> (SphinxHeader, Vec<SharedKey>) {
     (SphinxHeader {}, vec![])
 }
 
+fn gen_sender_secret() -> Scalar {
+    let mut rng = rand_os::OsRng::new().unwrap();
+    Scalar::random(&mut rng)
+}
+
 // We may be able to switch from Vec to array types as an optimization,
 // as in theory everything will have a constant size which we already know.
 // For now we'll stick with Vecs.
@@ -53,8 +79,11 @@ fn create_enc_payload(payload: Vec<u8>, shared_keys: Vec<SharedKey>) -> Vec<u8> 
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn gen_sender_secret_returns_a_scalar() {
+        let secret = gen_sender_secret();
+        assert!(secret.to_bytes().len() == 32);
     }
 }
