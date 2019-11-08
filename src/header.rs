7387c5eea83062acef0765a3fe58ec77c72b3a2d
Jedrzej Stuczynski <jedrzej.stuczynski@gmail.com>
2019-11-08 10:43:29 +0000

    Function to xor byte slice in place

--- src/header.rs
+++ src/header.rs
@@ -76,6 +76,16 @@ fn xor(a: &[u8], b: &[u8]) -> Vec<u8> {
     a.iter().zip(b.iter()).map(|(&x1, &x2)| x1 ^ x2).collect()
 }
 
+// xor_with xors assigns the result of xor to the first argument
+fn xor_with(a: &mut [u8], b: &[u8]) {
+    assert_eq!(a.len(), b.len());
+
+    a.iter_mut()
+        .zip(b.iter())
+        .map(|(x1, &x2)| *x1 ^= x2)
+        .collect()
+}
+
 fn create_zero_bytes(length: usize) -> Vec<u8> {
     vec![0; length]
 }
@@ -478,6 +488,38 @@ speculate! {
                 let a: Vec<u8> = vec![1, 2, 3];
                 let b: Vec<u8> = vec![4, 5];
                 let c = xor(&a, &b);
+            }
+        }
+    }
+
+    describe "xor_with" {
+        context "for empty inputs" {
+            it "does not change initial value" {
+                let mut a: Vec<u8> = vec![];
+                let b: Vec<u8> = vec![];
+                xor_with(&mut a, &b);
+                assert_eq!(0, a.len());
+            }
+        }
+
+        context "for non-zero inputs of same length" {
+            it "returns the expected xor of the vectors" {
+                let mut a: Vec<u8> = vec![1, 2, 3];
+                let b: Vec<u8> = vec![4, 5, 6];
+                xor_with(&mut a, &b);
+                assert_eq!(1^4, a[0]);
+                assert_eq!(2^5, a[1]);
+                assert_eq!(3^6, a[2]);
+
+            }
+        }
+
+        context "for inputs of different lengths" {
+            #[should_panic]
+            it "panics" {
+                let mut a: Vec<u8> = vec![1, 2, 3];
+                let b: Vec<u8> = vec![4, 5];
+                xor_with(&mut a, &b);
             }
         }
     }
