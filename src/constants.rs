0deec6c28d95b1997cf6effa098d174b2d824c34
Dave Hrycyszyn <futurechimp@users.noreply.github.com>
2019-11-13 16:38:14 +0000

    Documenting a few constants

--- src/constants.rs
+++ src/constants.rs
@@ -2,8 +2,12 @@ use crate::utils::crypto;
 
 // AVERAGE_DELAY SHOULD NEVER BE ZERO!!
 pub const AVERAGE_DELAY: f64 = 10.0;
-pub const SECURITY_PARAMETER: usize = 16; // in bytes; 128 bits
-pub const MAX_PATH_LENGTH: usize = 5; // what we refer in the Sphinx paper as r
+
+// k in the Sphinx paper. Measured in bytes; 128 bits.
+pub const SECURITY_PARAMETER: usize = 16;
+
+// r in the Sphinx paper
+pub const MAX_PATH_LENGTH: usize = 5;
 pub const ROUTING_KEYS_LENGTH: usize = 2 * crypto::STREAM_CIPHER_KEY_SIZE;
 pub const HKDF_INPUT_SEED: &[u8; 97] = b"Dwste mou enan moxlo arketa makru kai ena upomoxlio gia na ton topothetisw kai tha kinisw thn gh.";
 pub const STREAM_CIPHER_OUTPUT_LENGTH: usize = (2 * MAX_PATH_LENGTH + 3) * SECURITY_PARAMETER;
