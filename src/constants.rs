1e77dea401231b7d3209f5670d879c146f9e7476
Dave Hrycyszyn <futurechimp@users.noreply.github.com>
2019-11-13 16:38:26 +0000

    formatting

--- src/constants.rs
+++ src/constants.rs
@@ -8,6 +8,7 @@ pub const SECURITY_PARAMETER: usize = 16;
 
 // r in the Sphinx paper
 pub const MAX_PATH_LENGTH: usize = 5;
+
 pub const ROUTING_KEYS_LENGTH: usize = 2 * crypto::STREAM_CIPHER_KEY_SIZE;
 pub const HKDF_INPUT_SEED: &[u8; 97] = b"Dwste mou enan moxlo arketa makru kai ena upomoxlio gia na ton topothetisw kai tha kinisw thn gh.";
 pub const STREAM_CIPHER_OUTPUT_LENGTH: usize = (2 * MAX_PATH_LENGTH + 3) * SECURITY_PARAMETER;
