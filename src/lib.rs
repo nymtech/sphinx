a3cf5c2b4c39d731d5638d21b0606e4ea3b0a729
Dave Hrycyszyn <futurechimp@users.noreply.github.com>
2019-11-22 17:29:06 +0000

    Renamed create to new

--- src/lib.rs
+++ src/lib.rs
@@ -15,17 +15,16 @@ pub struct SphinxPacket {
 }
 
 impl SphinxPacket {
-    pub fn create(message: Vec<u8>, route: &[Node], destination: &Destination) -> SphinxPacket {
+    pub fn new(message: Vec<u8>, route: &[Node], destination: &Destination) -> SphinxPacket {
         let initial_secret = crypto::generate_secret();
-        let (header, payload_keys) = header::create(initial_secret, route, destination);
+        let (header, payload_keys) = header::SphinxHeader::new(initial_secret, route, destination);
         let payload = payload::create(&message, payload_keys, destination.address);
         SphinxPacket { header, payload }
     }
 
-    // TODO: needs the processor's secret key somehow, so far I'm just passing it the return value could also be a message, handle this
     // TODO: we should have some list of 'seen shared_keys' for replay detection, but this should be handled by a mix node
     pub fn process(self, node_secret_key: Scalar) -> (SphinxPacket, NodeAddressBytes) {
-        let unwrapped_header = header::process_header(self.header, node_secret_key).unwrap();
+        let unwrapped_header = self.header.process(node_secret_key).unwrap();
         let (new_header, next_hop_address, payload_key) = unwrapped_header;
 
         // process the payload
