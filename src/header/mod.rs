270658df1ebd8cf830b36da463410d3bbd962322
Dave Hrycyszyn <futurechimp@users.noreply.github.com>
2019-11-22 17:28:28 +0000

    Moving header methods into impl

--- src/header/mod.rs
+++ src/header/mod.rs
@@ -24,77 +24,83 @@ pub enum SphinxUnwrapError {
     IntegrityMacError,
 }
 
-// needs client's secret key, how should we inject this?
-// needs to deal with SURBs too at some point
-pub fn create(
-    initial_secret: Scalar,
-    route: &[Node],
-    destination: &Destination,
-) -> (SphinxHeader, Vec<PayloadKey>) {
-    let key_material = keys::KeyMaterial::derive(route, initial_secret);
-    let _ = delays::generate(route.len());
-    let filler_string = Filler::new(&key_material.routing_keys[..route.len() - 1]);
-    let routing_info = routing::EncapsulatedRoutingInformation::new(
-        route,
-        destination,
-        &key_material.routing_keys,
-        filler_string,
-    );
+impl SphinxHeader {
+    // needs client's secret key, how should we inject this?
+    // needs to deal with SURBs too at some point
+    pub fn new(
+        initial_secret: Scalar,
+        route: &[Node],
+        destination: &Destination,
+    ) -> (SphinxHeader, Vec<PayloadKey>) {
+        let key_material = keys::KeyMaterial::derive(route, initial_secret);
+        let _ = delays::generate(route.len());
+        let filler_string = Filler::new(&key_material.routing_keys[..route.len() - 1]);
+        let routing_info = routing::EncapsulatedRoutingInformation::new(
+            route,
+            destination,
+            &key_material.routing_keys,
+            filler_string,
+        );
 
-    // encapsulate header.routing information, compute MACs
-    (
-        SphinxHeader {
-            shared_secret: key_material.initial_shared_secret,
-            routing_info,
-        },
-        key_material
-            .routing_keys
-            .iter()
-            .map(|routing_key| routing_key.payload_key)
-            .collect(),
-    )
-}
-
-pub fn process_header(
-    header: SphinxHeader,
-    node_secret_key: Scalar,
-) -> Result<(SphinxHeader, NodeAddressBytes, PayloadKey), SphinxUnwrapError> {
-    let shared_secret = header.shared_secret;
-    let shared_key = keys::KeyMaterial::compute_shared_key(shared_secret, &node_secret_key);
-    let routing_keys = keys::RoutingKeys::derive(shared_key);
-
-    if !header.routing_info.integrity_mac.verify(
-        routing_keys.header_integrity_hmac_key,
-        header.routing_info.enc_routing_information.get_value_ref(),
-    ) {
-        return Err(SphinxUnwrapError::IntegrityMacError);
+        // encapsulate header.routing information, compute MACs
+        (
+            SphinxHeader {
+                shared_secret: key_material.initial_shared_secret,
+                routing_info,
+            },
+            key_material
+                .routing_keys
+                .iter()
+                .map(|routing_key| routing_key.payload_key)
+                .collect(),
+        )
     }
 
-    let (next_hop_address, encapsulated_next_hop) = unwrap::unwrap_routing_information(
-        header.routing_info.enc_routing_information,
-        routing_keys.stream_cipher_key,
-    );
+    pub fn process(
+        self,
+        node_secret_key: Scalar,
+    ) -> Result<(SphinxHeader, NodeAddressBytes, PayloadKey), SphinxUnwrapError> {
+        let shared_secret = self.shared_secret;
+        let shared_key = keys::KeyMaterial::compute_shared_key(shared_secret, &node_secret_key);
+        let routing_keys = keys::RoutingKeys::derive(shared_key);
 
-    // blind the shared_secret in the header
-    let new_shared_secret = blind_the_shared_secret(shared_secret, shared_key);
+        if !self.routing_info.integrity_mac.verify(
+            routing_keys.header_integrity_hmac_key,
+            self.routing_info.enc_routing_information.get_value_ref(),
+        ) {
+            return Err(SphinxUnwrapError::IntegrityMacError);
+        }
 
-    let new_header = SphinxHeader {
-        shared_secret: new_shared_secret,
-        routing_info: encapsulated_next_hop,
-    };
+        // blind the shared_secret in the header
+        let new_shared_secret = self.blind_the_shared_secret(shared_secret, shared_key);
 
-    Ok((new_header, next_hop_address, routing_keys.payload_key))
-}
+        let (next_hop_address, encapsulated_next_hop) = unwrap::unwrap_routing_information(
+            self.routing_info.enc_routing_information,
+            routing_keys.stream_cipher_key,
+        );
 
-fn blind_the_shared_secret(shared_secret: PublicKey, shared_key: SharedKey) -> PublicKey {
-    let hmac_full = compute_keyed_hmac(
-        shared_secret.to_bytes().to_vec(),
-        &shared_key.to_bytes().to_vec(),
-    );
-    let mut hmac = [0u8; 32];
-    hmac.copy_from_slice(&hmac_full[..32]);
-    let blinding_factor = Scalar::from_bytes_mod_order(hmac);
-    shared_secret * blinding_factor
+        let new_header = SphinxHeader {
+            shared_secret: new_shared_secret,
+            routing_info: encapsulated_next_hop,
+        };
+
+        Ok((new_header, next_hop_address, routing_keys.payload_key))
+    }
+
+    fn blind_the_shared_secret(
+        &self,
+        shared_secret: PublicKey,
+        shared_key: SharedKey,
+    ) -> PublicKey {
+        let hmac_full = compute_keyed_hmac(
+            shared_secret.to_bytes().to_vec(),
+            &shared_key.to_bytes().to_vec(),
+        );
+        let mut hmac = [0u8; 32];
+        hmac.copy_from_slice(&hmac_full[..32]);
+        let blinding_factor = Scalar::from_bytes_mod_order(hmac);
+        shared_secret * blinding_factor
+    }
 }
 
 #[cfg(test)]
@@ -124,15 +130,15 @@ mod create_and_process_sphinx_packet_header {
         let route = [node1, node2, node3];
         let destination = destination_fixture();
         let initial_secret = crypto::generate_secret();
-        let (sphinx_header, _) = create(initial_secret, &route, &destination);
+        let (sphinx_header, _) = SphinxHeader::new(initial_secret, &route, &destination);
 
-        let (new_header, next_hop_address, _) = process_header(sphinx_header, node1_sk).unwrap();
+        let (new_header, next_hop_address, _) = sphinx_header.process(node1_sk).unwrap();
         assert_eq!([4u8; NODE_ADDRESS_LENGTH], next_hop_address);
 
-        let (new_header2, next_hop_address2, _) = process_header(new_header, node2_sk).unwrap();
+        let (new_header2, next_hop_address2, _) = new_header.process(node2_sk).unwrap();
         assert_eq!([2u8; NODE_ADDRESS_LENGTH], next_hop_address2);
 
-        let (_, next_hop_address3, _) = process_header(new_header2, node3_sk).unwrap();
+        let (_, next_hop_address3, _) = new_header2.process(node3_sk).unwrap();
         assert_eq!(destination.address, next_hop_address3);
     }
 }
