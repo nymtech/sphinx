4515df5a5a91934b20e9da5f0f03bf3369d20001
Jedrzej Stuczynski <jedrzej.stuczynski@gmail.com>
2020-01-10 09:50:08 +0000

    Fixed length boundaries for NodeAddressBytes

--- src/route.rs
+++ src/route.rs
@@ -14,8 +14,8 @@ impl NodeAddressBytes {
 
     pub fn from_b64_string(value: String) -> Self {
         let decoded_address = base64::decode_config(&value, base64::URL_SAFE).unwrap();
-        assert_eq!(decoded_address.len(), 32);
-        let mut address_bytes = [0; 32];
+        assert_eq!(decoded_address.len(), NODE_ADDRESS_LENGTH);
+        let mut address_bytes = [0; NODE_ADDRESS_LENGTH];
         address_bytes.copy_from_slice(&decoded_address[..]);
 
         NodeAddressBytes(address_bytes)
