5ef88942e7d09905af350f1d008fc9203cf9a34a
Dave Hrycyszyn <futurechimp@users.noreply.github.com>
2020-02-25 15:20:33 +0000

    Optimising tests a bit

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -8,12 +8,11 @@ use sphinx::header::delays;
 use sphinx::route::NodeAddressBytes;
 use sphinx::route::{Destination, Node};
 use sphinx::SphinxPacket;
+use std::time::Duration;
 
 const NODE_ADDRESS_LENGTH: usize = 32;
 const DESTINATION_ADDRESS_LENGTH: usize = 32;
 const IDENTIFIER_LENGTH: usize = 16;
-const SECURITY_PARAMETER: usize = 16;
-const PAYLOAD_SIZE: usize = 1024;
 
 #[cfg(test)]
 mod tests {
@@ -23,16 +22,15 @@ mod tests {
     // two of those can be run concurrently to perform credential verification
     #[bench]
     fn bench_new(b: &mut Bencher) {
-        let (node1_sk, node1_pk) = crypto::keygen();
+        let (_, node1_pk) = crypto::keygen();
         let node1 = Node::new(NodeAddressBytes([5u8; NODE_ADDRESS_LENGTH]), node1_pk);
-        let (node2_sk, node2_pk) = crypto::keygen();
+        let (_, node2_pk) = crypto::keygen();
         let node2 = Node::new(NodeAddressBytes([4u8; NODE_ADDRESS_LENGTH]), node2_pk);
-        let (node3_sk, node3_pk) = crypto::keygen();
+        let (_, node3_pk) = crypto::keygen();
         let node3 = Node::new(NodeAddressBytes([2u8; NODE_ADDRESS_LENGTH]), node3_pk);
 
         let route = [node1, node2, node3];
-        let average_delay = 1.0;
-        let delays = delays::generate(route.len(), average_delay);
+        let delays = delays::generate_from_average_duration(route.len(), Duration::from_millis(10));
         let destination =
             Destination::new([3u8; DESTINATION_ADDRESS_LENGTH], [4u8; IDENTIFIER_LENGTH]);
 
@@ -46,14 +44,13 @@ mod tests {
     fn bench_unwrap(b: &mut Bencher) {
         let (node1_sk, node1_pk) = crypto::keygen();
         let node1 = Node::new(NodeAddressBytes([5u8; NODE_ADDRESS_LENGTH]), node1_pk);
-        let (node2_sk, node2_pk) = crypto::keygen();
+        let (_, node2_pk) = crypto::keygen();
         let node2 = Node::new(NodeAddressBytes([4u8; NODE_ADDRESS_LENGTH]), node2_pk);
-        let (node3_sk, node3_pk) = crypto::keygen();
+        let (_, node3_pk) = crypto::keygen();
         let node3 = Node::new(NodeAddressBytes([2u8; NODE_ADDRESS_LENGTH]), node3_pk);
 
         let route = [node1, node2, node3];
-        let average_delay = 1.0;
-        let delays = delays::generate(route.len(), average_delay);
+        let delays = delays::generate_from_average_duration(route.len(), Duration::from_millis(10));
         let destination =
             Destination::new([3u8; DESTINATION_ADDRESS_LENGTH], [4u8; IDENTIFIER_LENGTH]);
 
