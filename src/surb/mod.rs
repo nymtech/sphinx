741d5e4e66b9004940119f0eb0a6c0811a44d413
aniampio <aniapiotrowska2607@gmail.com>
2020-04-07 15:20:08 +0100

    Implement SURB creation

--- src/surb/mod.rs
+++ src/surb/mod.rs
@@ -1,9 +1,48 @@
-use crate::header;
+use crate::header::delays::Delay;
 use crate::header::keys::PayloadKey;
-use crate::route::NodeAddressBytes;
+use crate::route::{Destination, Node, NodeAddressBytes};
+use crate::{crypto, header};
+use curve25519_dalek::scalar::Scalar;
 
+#[derive(Clone)]
 pub struct SURB {
+    /// A Single Use Reply Block (SURB) must have a pre-aggregated Sphinx header,
+    /// the address of the first hop in the route of the SURB, and the key material
+    /// used to layer encrypt the payload.
     pub SURBHeader: header::SphinxHeader,
     pub first_hop_address: NodeAddressBytes,
     pub payload_key_material: Vec<PayloadKey>,
+}
+
+#[derive(Debug)]
+pub enum SURBCreationError {
+    IncorrectSURBRoute,
+}
+
+impl SURB {
+    pub fn new(
+        surb_initial_secret: Scalar,
+        surb_route: &[Node],
+        surb_delays: &[Delay],
+        surb_destination: &Destination,
+    ) -> Result<Self, SURBCreationError> {
+        assert_eq!(surb_route.len(), surb_delays.len());
+
+        let (header, payload_keys) = header::SphinxHeader::new(
+            surb_initial_secret,
+            surb_route,
+            surb_delays,
+            surb_destination,
+        );
+
+        let first_hop = surb_route
+            .last()
+            .ok_or(SURBCreationError::IncorrectSURBRoute)?;
+
+        Ok(SURB {
+            SURBHeader: header,
+            first_hop_address: first_hop.address.clone(),
+            payload_key_material: payload_keys,
+        })
+    }
 }
