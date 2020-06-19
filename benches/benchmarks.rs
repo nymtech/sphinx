6d1cb504316a69f66c15687478de5f5fb2559787
jstuczyn <jedrzej.stuczynski@gmail.com>
2020-06-19 16:08:00 +0100

    Changed benchmark framework - no longer requires nightly

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -12,87 +12,98 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#![feature(test)]
-
 extern crate sphinx;
-extern crate test;
 
-use sphinx::crypto;
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use sphinx::constants::{DESTINATION_ADDRESS_LENGTH, IDENTIFIER_LENGTH, NODE_ADDRESS_LENGTH};
+use sphinx::crypto::keygen;
 use sphinx::header::delays;
 use sphinx::route::{Destination, DestinationAddressBytes, Node, NodeAddressBytes};
 use sphinx::SphinxPacket;
 use std::time::Duration;
 
-const NODE_ADDRESS_LENGTH: usize = 32;
-const DESTINATION_ADDRESS_LENGTH: usize = 32;
-const IDENTIFIER_LENGTH: usize = 16;
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use test::Bencher;
-
-    // two of those can be run concurrently to perform credential verification
-    #[bench]
-    fn bench_new_no_surb(b: &mut Bencher) {
-        let (_, node1_pk) = crypto::keygen();
-        let node1 = Node::new(
-            NodeAddressBytes::from_bytes([5u8; NODE_ADDRESS_LENGTH]),
-            node1_pk,
-        );
-        let (_, node2_pk) = crypto::keygen();
-        let node2 = Node::new(
-            NodeAddressBytes::from_bytes([4u8; NODE_ADDRESS_LENGTH]),
-            node2_pk,
-        );
-        let (_, node3_pk) = crypto::keygen();
-        let node3 = Node::new(
-            NodeAddressBytes::from_bytes([2u8; NODE_ADDRESS_LENGTH]),
-            node3_pk,
-        );
-
-        let route = [node1, node2, node3];
-        let delays = delays::generate_from_average_duration(route.len(), Duration::from_millis(10));
-        let destination = Destination::new(
-            DestinationAddressBytes::from_bytes([3u8; DESTINATION_ADDRESS_LENGTH]),
-            [4u8; IDENTIFIER_LENGTH],
-        );
-
-        let message = vec![13u8, 16];
-        b.iter(|| {
-            SphinxPacket::new(message.clone(), &route, &destination, &delays).unwrap();
-        })
-    }
-
-    #[bench]
-    fn bench_unwrap(b: &mut Bencher) {
-        let (node1_sk, node1_pk) = crypto::keygen();
-        let node1 = Node::new(
-            NodeAddressBytes::from_bytes([5u8; NODE_ADDRESS_LENGTH]),
-            node1_pk,
-        );
-        let (_, node2_pk) = crypto::keygen();
-        let node2 = Node::new(
-            NodeAddressBytes::from_bytes([4u8; NODE_ADDRESS_LENGTH]),
-            node2_pk,
-        );
-        let (_, node3_pk) = crypto::keygen();
-        let node3 = Node::new(
-            NodeAddressBytes::from_bytes([2u8; NODE_ADDRESS_LENGTH]),
-            node3_pk,
-        );
-
-        let route = [node1, node2, node3];
-        let delays = delays::generate_from_average_duration(route.len(), Duration::from_millis(10));
-        let destination = Destination::new(
-            DestinationAddressBytes::from_bytes([3u8; DESTINATION_ADDRESS_LENGTH]),
-            [4u8; IDENTIFIER_LENGTH],
-        );
-
-        let message = vec![13u8, 16];
-        let packet = SphinxPacket::new(message.clone(), &route, &destination, &delays).unwrap();
-        b.iter(|| {
-            packet.clone().process(&node1_sk).unwrap();
-        })
-    }
+fn make_packet_copy(packet: &SphinxPacket) -> SphinxPacket {
+    SphinxPacket::from_bytes(&packet.to_bytes()).unwrap()
 }
+
+// two of those can be run concurrently to perform credential verification
+fn bench_new_no_surb(c: &mut Criterion) {
+    let (_, node1_pk) = keygen();
+    let node1 = Node::new(
+        NodeAddressBytes::from_bytes([5u8; NODE_ADDRESS_LENGTH]),
+        node1_pk,
+    );
+    let (_, node2_pk) = keygen();
+    let node2 = Node::new(
+        NodeAddressBytes::from_bytes([4u8; NODE_ADDRESS_LENGTH]),
+        node2_pk,
+    );
+    let (_, node3_pk) = keygen();
+    let node3 = Node::new(
+        NodeAddressBytes::from_bytes([2u8; NODE_ADDRESS_LENGTH]),
+        node3_pk,
+    );
+
+    let route = [node1, node2, node3];
+    let delays = delays::generate_from_average_duration(route.len(), Duration::from_millis(10));
+    let destination = Destination::new(
+        DestinationAddressBytes::from_bytes([3u8; DESTINATION_ADDRESS_LENGTH]),
+        [4u8; IDENTIFIER_LENGTH],
+    );
+
+    let message = vec![13u8, 16];
+
+    c.bench_function("sphinx creation", |b| {
+        b.iter(|| {
+            SphinxPacket::new(
+                black_box(message.clone()),
+                black_box(&route),
+                black_box(&destination),
+                black_box(&delays),
+            )
+            .unwrap()
+        })
+    });
+}
+
+fn bench_unwrap(c: &mut Criterion) {
+    let (node1_sk, node1_pk) = keygen();
+    let node1 = Node::new(
+        NodeAddressBytes::from_bytes([5u8; NODE_ADDRESS_LENGTH]),
+        node1_pk,
+    );
+    let (_, node2_pk) = keygen();
+    let node2 = Node::new(
+        NodeAddressBytes::from_bytes([4u8; NODE_ADDRESS_LENGTH]),
+        node2_pk,
+    );
+    let (_, node3_pk) = keygen();
+    let node3 = Node::new(
+        NodeAddressBytes::from_bytes([2u8; NODE_ADDRESS_LENGTH]),
+        node3_pk,
+    );
+
+    let route = [node1, node2, node3];
+    let delays = delays::generate_from_average_duration(route.len(), Duration::from_millis(10));
+    let destination = Destination::new(
+        DestinationAddressBytes::from_bytes([3u8; DESTINATION_ADDRESS_LENGTH]),
+        [4u8; IDENTIFIER_LENGTH],
+    );
+
+    let message = vec![13u8, 16];
+    let packet = SphinxPacket::new(message, &route, &destination, &delays).unwrap();
+
+    // technically it's not benching only unwrapping, but also "make_packet_copy"
+    // but it's relatively small
+    c.bench_function("sphinx unwrap", |b| {
+        b.iter(|| {
+            make_packet_copy(&packet)
+                .process(black_box(&node1_sk))
+                .unwrap()
+        })
+    });
+}
+
+criterion_group!(sphinx, bench_new_no_surb, bench_unwrap);
+
+criterion_main!(sphinx);
