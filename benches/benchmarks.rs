fb32c0b5050d01d027c81e7407554d51e630b841
aniampio <anna.piotrowska.15@ucl.ac.uk>
2021-05-05 15:25:10 +0100

    Add benchmark on key reuse unwrap function

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -18,8 +18,10 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use sphinx::constants::{
     DESTINATION_ADDRESS_LENGTH, HKDF_SALT_SIZE, IDENTIFIER_LENGTH, NODE_ADDRESS_LENGTH,
 };
-use sphinx::crypto::keygen;
-use sphinx::header::delays;
+use sphinx::crypto::{keygen, EphemeralSecret, SharedSecret};
+use sphinx::header::{delays, SphinxHeader};
+use sphinx::packet::builder::DEFAULT_PAYLOAD_SIZE;
+use sphinx::payload::Payload;
 use sphinx::route::{Destination, DestinationAddressBytes, Node, NodeAddressBytes};
 use sphinx::SphinxPacket;
 use std::time::Duration;
@@ -118,6 +120,64 @@ fn bench_unwrap(c: &mut Criterion) {
     });
 }
 
-criterion_group!(sphinx, bench_new_no_surb, bench_unwrap);
+fn bench_unwrap_key_reuse(c: &mut Criterion) {
+    let (node1_sk, node1_pk) = keygen();
+    let node1 = Node::new(
+        NodeAddressBytes::from_bytes([5u8; NODE_ADDRESS_LENGTH]),
+        node1_pk,
+    );
+    let (_, node2_pk) = keygen();
+    let node2 = Node::new(
+        NodeAddressBytes::from_bytes([4u8; NODE_ADDRESS_LENGTH]),
+        node2_pk,
+    );
+    let (_, node3_pk) = keygen();
+    let node3 = Node::new(
+        NodeAddressBytes::from_bytes([2u8; NODE_ADDRESS_LENGTH]),
+        node3_pk,
+    );
+
+    let route = [node1, node2, node3];
+    let delays = delays::generate_from_average_duration(route.len(), Duration::from_millis(10));
+    let destination = Destination::new(
+        DestinationAddressBytes::from_bytes([3u8; DESTINATION_ADDRESS_LENGTH]),
+        [4u8; IDENTIFIER_LENGTH],
+    );
+    let hkdf_salt = [
+        [4u8; HKDF_SALT_SIZE],
+        [1u8; HKDF_SALT_SIZE],
+        [3u8; HKDF_SALT_SIZE],
+    ];
+    let initial_secret = EphemeralSecret::new();
+    let initial_shared_secret = SharedSecret::from(&initial_secret);
+    let message = vec![13u8, 16];
+    let (header, payload_keys) =
+        SphinxHeader::new(&initial_secret, &route, &delays, &hkdf_salt, &destination);
+    let payload = Payload::encapsulate_message(&message, &payload_keys, DEFAULT_PAYLOAD_SIZE);
+    let payload = match payload {
+        Ok(payload) => payload,
+        Err(error) => panic!("Problem when encapsulating message: {:?}", error),
+    };
+
+    let packet = SphinxPacket { header, payload };
+    let shared_key1 = node1_sk.diffie_hellman(&initial_shared_secret);
+
+    c.bench_function("sphinx unwrap key reuse", |b| {
+        b.iter(|| {
+            make_packet_copy(&packet)
+                .process_with_previously_derived_keys(
+                    black_box(shared_key1),
+                    black_box(Some(&hkdf_salt[0])),
+                )
+                .unwrap()
+        })
+    });
+}
+criterion_group!(
+    sphinx,
+    bench_new_no_surb,
+    bench_unwrap,
+    bench_unwrap_key_reuse
+);
 
 criterion_main!(sphinx);
