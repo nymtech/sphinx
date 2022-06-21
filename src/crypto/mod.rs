0ebb368b18f5a60ca6b796fe7777dce5228333b5
vnpmid <dhani.prg.2007@gmail.com>
2022-06-21 09:48:55 +0700

    Improve aes & hmac

--- src/crypto/mod.rs
+++ src/crypto/mod.rs
@@ -12,11 +12,26 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use aes::cipher::{NewCipher, StreamCipher};
-use aes::Aes128Ctr;
-use digest::generic_array::{ArrayLength, GenericArray};
-use digest::{BlockInput, FixedOutput, Reset, Update};
-use hmac::{crypto_mac, Hmac, Mac, NewMac};
+use aes::cipher::{KeyIvInit, StreamCipher};
+/*
+use digest::{
+	Mac,
+	crypto_common::BlockSizeUser, CtOutput, Digest
+};
+use hmac::SimpleHmac;
+*/
+
+//Hmac not supported with black3
+//https://githubhot.com/repo/BLAKE3-team/BLAKE3/issues/223
+use digest::{
+	Mac,
+	CtOutput,
+	HashMarker,
+	core_api::{CoreWrapper, BlockSizeUser, OutputSizeUser, CoreProxy, UpdateCore, FixedOutputCore, BufferKindUser},
+	generic_array::{ArrayLength, typenum::{IsLess, Le, NonZero, U256}},
+	block_buffer::Eager,
+};
+use hmac::{Hmac, HmacCore};
 
 pub mod keys;
 
@@ -27,7 +42,18 @@ pub const STREAM_CIPHER_KEY_SIZE: usize = 16;
 pub const STREAM_CIPHER_INIT_VECTOR: [u8; 16] = [0u8; 16];
 
 // Type alias for ease of use so that it would not require explicit import of crypto_mac or Hmac
-pub type HmacOutput<D> = crypto_mac::Output<Hmac<D>>;
+/*
+pub type HmacOutput<D> = CtOutput<SimpleHmac<D>>;
+*/
+
+//Hmac not supported with black3
+//https://githubhot.com/repo/BLAKE3-team/BLAKE3/issues/223
+pub type HmacOutput<D> = CtOutput<CoreWrapper<HmacCore<D>>>;
+
+//&GenericArray<u8, <<D as CoreProxy>::Core as OutputSizeUser>::OutputSize>
+
+// Type alias for Aes128 ctr mode
+type Aes128Ctr = ctr::Ctr128BE<aes::Aes128>;
 
 pub fn generate_pseudorandom_bytes(
     // TODO: those should use proper generic arrays to begin with!!
@@ -36,22 +62,40 @@ pub fn generate_pseudorandom_bytes(
     iv: &[u8; STREAM_CIPHER_KEY_SIZE],
     length: usize,
 ) -> Vec<u8> {
-    let cipher_key = GenericArray::from_slice(&key[..]);
-    let cipher_nonce = GenericArray::from_slice(&iv[..]);
-
     // generate a random string as an output of a PRNG, which we implement using stream cipher AES_CTR
-    let mut cipher = Aes128Ctr::new(cipher_key, cipher_nonce);
-    let mut data = vec![0u8; length];
-    cipher.apply_keystream(&mut data);
-    data
+    let mut cipher = Aes128Ctr::new(key.into(), iv.into());
+	let mut data = vec![0u8; length];
+	cipher.apply_keystream(&mut data);
+	data
 }
 
 /// Compute keyed hmac
+/*
 pub fn compute_keyed_hmac<D>(key: &[u8], data: &[u8]) -> HmacOutput<D>
 where
-    D: Update + BlockInput + FixedOutput + Reset + Default + Clone,
-    D::BlockSize: ArrayLength<u8>,
-    D::OutputSize: ArrayLength<u8>,
+    D: Digest + BlockSizeUser,
+{
+    let mut hmac = SimpleHmac::<D>::new_from_slice(key)
+        .expect("HMAC was instantiated with a key of an invalid size!");
+    hmac.update(data);
+    hmac.finalize()
+}
+*/
+
+//Hmac not supported with black3
+//https://githubhot.com/repo/BLAKE3-team/BLAKE3/issues/223
+pub fn compute_keyed_hmac<D>(key: &[u8], data: &[u8]) -> HmacOutput<D>
+where
+    D: CoreProxy,
+    D::Core: HashMarker
+        + UpdateCore
+        + FixedOutputCore
+        + BufferKindUser<BufferKind = Eager>
+        + Default
+        + Clone,
+    <D::Core as BlockSizeUser>::BlockSize: IsLess<U256>,
+    Le<<D::Core as BlockSizeUser>::BlockSize, U256>: NonZero,
+	<D::Core as OutputSizeUser>::OutputSize: ArrayLength<u8>,
 {
     let mut hmac =
         Hmac::<D>::new_from_slice(key).expect("HMAC should be able to take key of any size!");
