756bf58f53f00aa95d79b0c282d69d71fcb6327b
aniampio <anna.piotrowska.15@ucl.ac.uk>
2023-05-03 14:45:42 +0100

    Update benchmarks

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -68,7 +68,10 @@ fn bench_sphinx_header(c: &mut Criterion){
 
     c.bench_function("Sphinx Header Creation", |b| {
         b.iter(|| {
-            SphinxHeader::new(&EphemeralSecret::new(), &route, &delays, &destination)
+            SphinxHeader::new(black_box(&EphemeralSecret::new()),
+                              black_box(&route),
+                              black_box(&delays),
+                              black_box(&destination))
         })
     });
 
@@ -76,13 +79,107 @@ fn bench_sphinx_header(c: &mut Criterion){
 
     c.bench_function("Sphinx Header Processing", |b| {
         b.iter(|| {
-            make_header_copy(&header).process(&node1_sk).unwrap()
+            make_header_copy(&header).process(black_box(&node1_sk)).unwrap()
         })
     });
 
 }
 
 fn bench_sphinx_payload(c: &mut Criterion){
+    let case = PayloadBenchCase {
+        payload_size: 1000,
+    };
+
+    let (_, node1_pk) = keygen();
+    let node1 = Node::new(
+        NodeAddressBytes::from_bytes([5u8; NODE_ADDRESS_LENGTH]),
+        node1_pk,
+    );
+    let (_, node2_pk) = keygen();
+    let node2 = Node::new(
+        NodeAddressBytes::from_bytes([4u8; NODE_ADDRESS_LENGTH]),
+        node2_pk,
+    );
+    let (_, node3_pk) = keygen();
+    let node3 = Node::new(
+        NodeAddressBytes::from_bytes([2u8; NODE_ADDRESS_LENGTH]),
+        node3_pk,
+    );
+
+    let route = [node1, node2, node3];
+    let delays = delays::generate_from_average_duration(route.len(), Duration::from_millis(10));
+    let destination = Destination::new(
+        DestinationAddressBytes::from_bytes([3u8; DESTINATION_ADDRESS_LENGTH]),
+        [4u8; IDENTIFIER_LENGTH],
+    );
+
+    let (_, payload_keys) = SphinxHeader::new(&EphemeralSecret::new(), &route, &delays, &destination);
+    let message = vec![1u8, case.payload_size as u8];
+
+    c.bench_function("Sphinx Payload Creation", |b| {
+        b.iter(|| {
+            Payload::encapsulate_message(black_box(message.as_ref()),
+                                         black_box(&payload_keys),
+                                         black_box(case.payload_size)).unwrap()
+        })
+    });
+
+    let payload =
+        Payload::encapsulate_message(message.as_ref(), &payload_keys, case.payload_size).unwrap();
+
+    let payload_key = payload_keys.first().unwrap();
+    c.bench_function("Sphinx Payload Processing", |b| {
+        b.iter(|| {
+            payload.clone().unwrap(black_box(&payload_key))
+        })
+    });
+}
+
+// two of those can be run concurrently to perform credential verification
+fn bench_new_no_surb(c: &mut Criterion) {
+    let case = PayloadBenchCase {
+        payload_size: 1000,
+    };
+
+    let (_, node1_pk) = keygen();
+    let node1 = Node::new(
+        NodeAddressBytes::from_bytes([5u8; NODE_ADDRESS_LENGTH]),
+        node1_pk,
+    );
+    let (_, node2_pk) = keygen();
+    let node2 = Node::new(
+        NodeAddressBytes::from_bytes([4u8; NODE_ADDRESS_LENGTH]),
+        node2_pk,
+    );
+    let (_, node3_pk) = keygen();
+    let node3 = Node::new(
+        NodeAddressBytes::from_bytes([2u8; NODE_ADDRESS_LENGTH]),
+        node3_pk,
+    );
+
+    let route = [node1, node2, node3];
+    let delays = delays::generate_from_average_duration(route.len(), Duration::from_millis(10));
+    let destination = Destination::new(
+        DestinationAddressBytes::from_bytes([3u8; DESTINATION_ADDRESS_LENGTH]),
+        [4u8; IDENTIFIER_LENGTH],
+    );
+
+    let message = vec![1u8, case.payload_size as u8];
+
+    c.bench_function("Sphinx Packet Creation - no SURBs", |b| {
+        b.iter(|| {
+            SphinxPacket::new(
+                black_box(message.clone()),
+                black_box(&route),
+                black_box(&destination),
+                black_box(&delays),
+            )
+            .unwrap()
+        })
+    });
+}
+
+fn bench_unwrap(c: &mut Criterion) {
     let case = PayloadBenchCase {
         payload_size: 1000,
     };
@@ -110,91 +207,7 @@ fn bench_sphinx_payload(c: &mut Criterion){
         [4u8; IDENTIFIER_LENGTH],
     );
 
-    let (_, payload_keys) = SphinxHeader::new(&EphemeralSecret::new(), &route, &delays, &destination);
-    let message = vec![1u8, 16];
-
-    c.bench_function("Sphinx Payload Creation", |b| {
-        b.iter(|| {
-            Payload::encapsulate_message(message.as_ref(), &payload_keys, case.payload_size).unwrap()
-        })
-    });
-
-    let payload =
-        Payload::encapsulate_message(message.as_ref(), &payload_keys, case.payload_size).unwrap();
-
-    let payload_key = payload_keys.first().unwrap();
-    c.bench_function("Sphinx Payload Processing", |b| {
-        b.iter(|| {
-            payload.clone().unwrap(&payload_key)
-        })
-    });
-}
-
-// two of those can be run concurrently to perform credential verification
-fn bench_new_no_surb(c: &mut Criterion) {
-    let (_, node1_pk) = keygen();
-    let node1 = Node::new(
-        NodeAddressBytes::from_bytes([5u8; NODE_ADDRESS_LENGTH]),
-        node1_pk,
-    );
-    let (_, node2_pk) = keygen();
-    let node2 = Node::new(
-        NodeAddressBytes::from_bytes([4u8; NODE_ADDRESS_LENGTH]),
-        node2_pk,
-    );
-    let (_, node3_pk) = keygen();
-    let node3 = Node::new(
-        NodeAddressBytes::from_bytes([2u8; NODE_ADDRESS_LENGTH]),
-        node3_pk,
-    );
-
-    let route = [node1, node2, node3];
-    let delays = delays::generate_from_average_duration(route.len(), Duration::from_millis(10));
-    let destination = Destination::new(
-        DestinationAddressBytes::from_bytes([3u8; DESTINATION_ADDRESS_LENGTH]),
-        [4u8; IDENTIFIER_LENGTH],
-    );
-
-    let message = vec![13u8, 16];
-
-    c.bench_function("Sphinx Packet Creation - no SURBs", |b| {
-        b.iter(|| {
-            SphinxPacket::new(
-                black_box(message.clone()),
-                black_box(&route),
-                black_box(&destination),
-                black_box(&delays),
-            )
-            .unwrap()
-        })
-    });
-}
-
-fn bench_unwrap(c: &mut Criterion) {
-    let (node1_sk, node1_pk) = keygen();
-    let node1 = Node::new(
-        NodeAddressBytes::from_bytes([5u8; NODE_ADDRESS_LENGTH]),
-        node1_pk,
-    );
-    let (_, node2_pk) = keygen();
-    let node2 = Node::new(
-        NodeAddressBytes::from_bytes([4u8; NODE_ADDRESS_LENGTH]),
-        node2_pk,
-    );
-    let (_, node3_pk) = keygen();
-    let node3 = Node::new(
-        NodeAddressBytes::from_bytes([2u8; NODE_ADDRESS_LENGTH]),
-        node3_pk,
-    );
-
-    let route = [node1, node2, node3];
-    let delays = delays::generate_from_average_duration(route.len(), Duration::from_millis(10));
-    let destination = Destination::new(
-        DestinationAddressBytes::from_bytes([3u8; DESTINATION_ADDRESS_LENGTH]),
-        [4u8; IDENTIFIER_LENGTH],
-    );
-
-    let message = vec![13u8, 16];
+    let message = vec![1u8, case.payload_size as u8];
     let packet = SphinxPacket::new(message, &route, &destination, &delays).unwrap();
 
     // technically it's not benching only unwrapping, but also "make_packet_copy"
@@ -209,5 +222,5 @@ fn bench_unwrap(c: &mut Criterion) {
 }
 
 // criterion_group!(sphinx, bench_new_no_surb, bench_unwrap);
-criterion_group!(sphinx, bench_sphinx_header, bench_sphinx_payload);
+criterion_group!(sphinx, bench_sphinx_header, bench_sphinx_payload, bench_new_no_surb, bench_unwrap);
 criterion_main!(sphinx);
