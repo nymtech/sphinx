e535fe20d41a7723e739b030c258b857741a0f48
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2025-02-24 15:53:11 +0000

    feat: removed ability to create or process legacy packets (#100)

--- src/header/keys.rs
+++ src/header/keys.rs
@@ -19,7 +19,6 @@ use crate::constants::{
 use crate::crypto;
 use crate::crypto::STREAM_CIPHER_KEY_SIZE;
 use crate::route::Node;
-use curve25519_dalek::Scalar;
 use hkdf::Hkdf;
 use sha2::Sha256;
 use std::convert::TryInto;
@@ -130,41 +129,6 @@ impl KeyMaterial {
 
         Self {
             initial_shared_secret,
-            routing_keys,
-        }
-    }
-
-    #[deprecated]
-    pub fn derive_legacy(route: &[Node], initial_secret: &StaticSecret) -> Self {
-        let initial_secret_scalar = Scalar::from_bytes_mod_order(initial_secret.to_bytes());
-
-        let initial_shared_secret =
-            curve25519_dalek::MontgomeryPoint::mul_base(&initial_secret_scalar);
-
-        let mut routing_keys = Vec::with_capacity(route.len());
-
-        let mut accumulator = initial_secret_scalar;
-        for (i, node) in route.iter().enumerate() {
-            // pub^{a * b * ...}
-            let pk_mt = curve25519_dalek::MontgomeryPoint(node.pub_key.to_bytes());
-            let shared_key = pk_mt * accumulator;
-
-            let node_routing_keys = RoutingKeys::derive(PublicKey::from(shared_key.to_bytes()));
-
-            // it's not the last iteration
-            if i != route.len() + 1 {
-                // convert the blinding factor to a raw scalar and perform multiplication without
-                // any reduction (UNSAFE since we're not in ristretto)
-                let blinding_factor_scalar =
-                    &Scalar::from_bytes_mod_order(node_routing_keys.blinding_factor.to_bytes());
-
-                accumulator *= blinding_factor_scalar;
-            }
-
-            routing_keys.push(node_routing_keys);
-        }
-        Self {
-            initial_shared_secret: PublicKey::from(initial_shared_secret.0),
             routing_keys,
         }
     }
